f4a5a55272b2dac6cb67d49c6ee0464a2f88646a
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-05-14 18:49:08 +0100

    allow host

--- todo_backend_new/settings.py
+++ todo_backend_new/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = 'django-insecure-72w_&gu!!sgz9q#5n0zcsu4_gya1j0)@9zr8-zwe^)z4m3elof
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',
+                 'todo-backend-new-nqhg.onrender.com']
 
 
 # Application definition
