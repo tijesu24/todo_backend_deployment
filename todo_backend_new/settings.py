a92cce307b1a4de29fc6263c2cadab6d19bea493
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-05-14 18:46:58 +0100

    settings changed

--- todo_backend_new/settings.py
+++ todo_backend_new/settings.py
@@ -136,6 +136,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -143,7 +146,8 @@ STATIC_URL = '/static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-if not DEBUG:
+# if not DEBUG:
+if True:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
