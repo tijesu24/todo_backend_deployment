596d0acfc2bcac6f24799c3f9e85e8d2684934f2
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-05-15 00:41:05 +0100

    not sure why cors is not working

--- todo_backend_new/settings.py
+++ todo_backend_new/settings.py
@@ -166,6 +166,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
