54f16ac77eb67fbf5406227318da983ba874c29e
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-05-15 00:42:56 +0100

    cors adj

--- todo_backend_new/settings.py
+++ todo_backend_new/settings.py
@@ -158,13 +158,13 @@ if True:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-#     'http://127.0.0.1:3000',
-#     'http://0.0.0.0:3000'
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://0.0.0.0:3000'
+]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 
 REST_FRAMEWORK = {
