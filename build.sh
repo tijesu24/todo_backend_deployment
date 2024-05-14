80605fb64f542939af8c9db472447b27f9ddb5a7
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-05-15 00:13:25 +0100

    makemigrations

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py makemigrateions todo
+python manage.py makemigrations todo
 python manage.py migrate
