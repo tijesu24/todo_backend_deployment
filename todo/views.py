31cea2d83907fd0abf51b0a2885ebe1b5a88e589
Tijesunimi Adebiyi <tijesu24@gmail.com>
2024-05-15 09:42:55 +0100

    trying to see what's wrong with logout

--- todo/views.py
+++ todo/views.py
@@ -43,6 +43,7 @@ class LogoutView(APIView):
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
+            print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
